11eaf0b0aa5370b916fcc01fcac95e567ed89b98
APapeIsName <gms72901217@gmail.com>
2025-11-20 01:57:26 +0900

    [Test] StringBuilder vs String vs CharArray

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,78 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+//        testStringBuilder();
+//        testString();
+        testCharArray();
+    }
+
+    // time = 14
+    // memory = 1607344
+    private static void testStringBuilder() {
+        Runtime.getRuntime().gc();
+        LocalDateTime start = LocalDateTime.now();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 10000; i++) {
+            stringBuilder.append("Hello World");
+        }
+        System.out.print("stringBuilder = " + stringBuilder);
+        LocalDateTime end = LocalDateTime.now();
+        System.out.print("\ntime = " + Duration.between(start, end).toMillis());
+        System.out.println("\nmemory = " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
+    }
+
+    // time = 54
+    // memory = 37826496
+    private static void testString() {
+        Runtime.getRuntime().gc();
+        LocalDateTime start = LocalDateTime.now();
+        String string = "";
+        for (int i = 0; i < 10000; i++) {
+            string += "Hello World";
+        }
+        System.out.print("string = " + string);
+        LocalDateTime end = LocalDateTime.now();
+        System.out.print("\ntime = " + Duration.between(start, end).toMillis());
+        System.out.println("\nmemory = " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
+    }
+
+    // time = 90
+    // memory = 1636448
+    private static void testCharArray() {
+        Runtime.getRuntime().gc();
+        LocalDateTime start = LocalDateTime.now();
+        char[] charArray = new char[110000];
+        for (int i = 0; i < 10000; i = i + 11) {
+            charArray[i] = 'H';
+            charArray[i+1] = 'e';
+            charArray[i+2] = 'l';
+            charArray[i+3] = 'l';
+            charArray[i+4] = 'o';
+            charArray[i+5] = ' ';
+            charArray[i+6] = 'w';
+            charArray[i+7] = 'o';
+            charArray[i+8] = 'r';
+            charArray[i+9] = 'l';
+            charArray[i+10] = 'd';
+        }
+//        System.out.print("charArray = " + charArray.length);
+        for (int i = 0; i < 110000; i = i + 1) {
+            System.out.print(charArray[i]);
+        }
+//        for (char c : charArray) {
+//            System.out.print(c);
+//        }
+        LocalDateTime end = LocalDateTime.now();
+        System.out.print("\ntime = " + Duration.between(start, end).toMillis());
+        System.out.println("\nmemory = " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
+    }
+
+    private static void solve() {
         Scanner scanner = new Scanner(System.in);
         List<String> inputList = new ArrayList<>();
         boolean isInputFirst = false;
