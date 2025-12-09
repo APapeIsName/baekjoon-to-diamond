c96d84e647ab3b7ac5b46e58ea086c019e816296
APapeIsName <gms72901217@gmail.com>
2025-12-09 23:44:54 +0900

    1차 시도

--- src/Main.java
+++ src/Main.java
@@ -1,104 +1,36 @@
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        testStringBuilder();
-//        testString();
-//        testCharArray();
+    public static void main(String[] args) throws IOException {
+        first();
     }
 
-    // time = 2109000
-    // memory = 2542576
-    private static void testStringBuilder() {
-        Runtime.getRuntime().gc();
-        LocalDateTime start = LocalDateTime.now();
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < 10000; i++) {
-            stringBuilder.append("Hello World");
-        }
-        System.out.print(stringBuilder);
-        LocalDateTime end = LocalDateTime.now();
-        System.out.print("\ntime = " + Duration.between(start, end).toNanos());
-        System.out.println("\nmemory = " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
-    }
+    public static void first() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    // time = 58081000
-    // memory = 98422424
-    private static void testString() {
-        Runtime.getRuntime().gc();
-        LocalDateTime start = LocalDateTime.now();
-        String string = "";
-        for (int i = 0; i < 10000; i++) {
-            string += "Hello World";
-        }
-        System.out.print(string);
-        LocalDateTime end = LocalDateTime.now();
-        System.out.print("\ntime = " + Duration.between(start, end).toNanos());
-        System.out.println("\nmemory = " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
-    }
+        String read = reader.readLine();
+        StringBuilder readSb = new StringBuilder(read);
+        int cycleCount = 0;
+        boolean isFirst = true;
 
-    // time = 1990000
-    // memory = 2308856
-    private static void testCharArray() {
-        Runtime.getRuntime().gc();
-        LocalDateTime start = LocalDateTime.now();
-        char[] charArray = new char[110000];
-        for (int i = 0; i < 110000; i = i + 11) {
-            charArray[i] = 'H';
-            charArray[i+1] = 'e';
-            charArray[i+2] = 'l';
-            charArray[i+3] = 'l';
-            charArray[i+4] = 'o';
-            charArray[i+5] = ' ';
-            charArray[i+6] = 'w';
-            charArray[i+7] = 'o';
-            charArray[i+8] = 'r';
-            charArray[i+9] = 'l';
-            charArray[i+10] = 'd';
-        }
-//        for (int i = 0; i < 110000; i = i + 1) {
-//            System.out.print(charArray[i]);
-//        }
-        System.out.print(charArray);
-        LocalDateTime end = LocalDateTime.now();
-        System.out.print("\ntime = " + Duration.between(start, end).toNanos());
-        System.out.println("\nmemory = " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
-    }
-
-    private static void solve() {
-        Scanner scanner = new Scanner(System.in);
-        List<String> inputList = new ArrayList<>();
-        boolean isInputFirst = false;
-        byte inputNumber = 0;
-        while (!isInputFirst || inputNumber != 0) {
-            inputNumber--;
-            String inputString = scanner.nextLine();
-            if (!isInputFirst) {
-                inputNumber = Byte.parseByte(inputString);
-                isInputFirst = true;
-            } else {
-                inputList.add(inputString);
+        while (isFirst || !read.equals(readSb.toString())) {
+            if (readSb.length() < 2) {
+                readSb.insert(0, "0");
             }
-        }
-        char[] result = inputList.get(0).toCharArray();
-        for (int i = 0; i < inputList.size() - 1; i++) {
-            for (int j = 0; j < result.length; j++) {
-                char[] first = inputList.get(i).toCharArray();
-                char[] second = inputList.get(i + 1).toCharArray();
 
-                if (first[j] == second[j]) {
-                    if (result[j] != first[j]
-                            && result[j] != '?') result[j] = first[j];
-                } else {
-                    if (result[j] != '?') result[j] = '?';
-                }
-            }
+            int leftLastNum = readSb.charAt(1) - 48;
+            int sum = readSb.charAt(0) + leftLastNum - 48;
+
+            int res = (leftLastNum * 10) + (sum % 10);
+
+            readSb.delete(0, 2);
+            readSb.append(res);
+
+            cycleCount++;
+
+            isFirst = false;
         }
 
-        System.out.println(result);
+        System.out.print(cycleCount);
     }
 }
