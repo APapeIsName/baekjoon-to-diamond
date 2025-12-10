19f836814681f2cc08aca65cbf83f5a3f8f4edaf
APapeIsName <gms72901217@gmail.com>
2025-12-11 01:28:30 +0900

    문제 풀이 및 고쳐야 할 점 작성

--- src/Main.java
+++ src/Main.java
@@ -1,109 +1,46 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         first();
     }
 
-    // 256ms -> 최적화 필요
+    // 고쳐야 할 점 : 1.Scanner 는 느림. 정규식 검사 과정을 거치기 때문. BufferedReader 를 쓰자
+    // 2.지금은 계산식보다도 모양만 보는 알고리즘인데 int 를 사용하지 말고 String 으로 처리했어도 됐다.
     public static void first() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 1~99999 정수
+        Scanner sc = new Scanner(System.in);
+        StringBuilder res = new StringBuilder();
 
-        char[] chArr = br.readLine().toUpperCase().toCharArray();
-        int[] countArr = new int[26];
+        int n;
+        List<Integer> numList = new ArrayList<>();
 
-        int index;
-        for (char ch : chArr) {
-            index = ch - 65;
-            countArr[index]++;
-        }
-
-        int max = 0;
-        int maxIndex = 0;
-        for (int i = 0; i < 13; i++) {
-            int left = countArr[12 - i];
-            int right = countArr[13 + i];
-            if (left > max) {
-                max = left;
-                maxIndex = 12 - i;
+        while ((n = sc.nextInt()) != 0) {
+            while (n != 0) {
+                numList.add(n % 10);
+                n /= 10;
             }
-            if (right > max) {
-                max = right;
-                maxIndex = 13 + i;
+
+            boolean isEqual = true;
+            int listSize = numList.size();
+            for (int i = 0; i < listSize / 2; i++) {
+                isEqual = numList.get(i) == numList.get(listSize - 1 - i);
+                if (!isEqual) {
+                    break;
+                }
             }
-        }
 
-        for (int i = 0; i < 13; i++) {
-            int left = countArr[12 - i];
-            int right = countArr[13 + i];
-            if ((left == max && maxIndex != 12 - i) || (right == max && maxIndex != 13 + i)) {
-                System.out.print('?');
-                return;
-            }
-        }
-
-        System.out.print((char) (maxIndex + 65));
-    }
-
-    // 부족했던 점 : 1. System.in 사용 안 했던 거. 2. 단순히 26개 배열로 처리 가능했던 걸 놓쳤던 점
-    // 최적화 -> 132ms
-    public static void second() throws IOException {
-        int[] countArr = new int[26];
-
-        int c;
-        while((c = System.in.read()) > 64) {
-            if (c < 91) {
-                countArr[c - 65]++;
+            if (isEqual) {
+                res.append("yes\n");
             } else {
-                countArr[c - 97]++;
+                res.append("no\n");
             }
+
+            numList.clear();
         }
 
-        int max = 0;
-        int ch = -2;
-        for (int i = 0; i < 26; i++) {
-            if (countArr[i] > max) {
-                max = countArr[i];
-                ch = i;
-            }  else if (countArr[i] == max) {
-                ch = -2;
-            }
-        }
-
-        if (ch == -2) {
-            System.out.print('?');
-        } else {
-            System.out.print((char) (ch + 65));
-        }
-    }
-
-    // AI 피드백 확인
-    // + new 키워드 공부하기
-    public static void secondFixed() throws IOException {
-        int[] countArr = new int[26];
-
-        int c;
-        // isAlpha(c) 처럼 체크해도 되지만, 문제 조건상 알파벳만 들어오므로
-        // 개행문자(10) 전까지 읽는 것으로 처리해도 무방
-        while ((c = System.in.read()) >= 'A') {
-            // 1. 대소문자 구분 없이 소문자로 통일하여 카운팅 (비트 연산 활용)
-            // 'A' | 32 => 'a', 'a' | 32 => 'a'
-            countArr[(c | 32) - 'a']++;
-        }
-
-        int max = -1;
-        int maxIdx = -2; // ? 출력용 플래그
-
-        for (int i = 0; i < 26; i++) {
-            if (countArr[i] > max) {
-                max = countArr[i];
-                maxIdx = i;
-            } else if (countArr[i] == max) {
-                maxIdx = -2; // 중복 발생
-            }
-        }
-
-        // 삼항 연산자로 깔끔하게 출력
-        System.out.print(maxIdx == -2 ? '?' : (char) (maxIdx + 'A'));
+        System.out.print(res);
+        sc.close();
     }
 }
