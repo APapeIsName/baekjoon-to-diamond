fad6e14606446d4133b71db7e5fa6f5b2fb6715c
APapeIsName <gms72901217@gmail.com>
2025-12-11 01:39:21 +0900

    최적화 풀이

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        first();
+        second();
     }
 
+    // 172ms
     // 고쳐야 할 점 : 1.Scanner 는 느림. 정규식 검사 과정을 거치기 때문. BufferedReader 를 쓰자
     // 2.지금은 계산식보다도 모양만 보는 알고리즘인데 int 를 사용하지 말고 String 으로 처리했어도 됐다.
     public static void first() throws IOException {
@@ -42,5 +43,38 @@ public class Main {
 
         System.out.print(res);
         sc.close();
+    }
+
+    // 100ms
+    public static void second() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder res = new StringBuilder();
+
+        while (true) {
+            String s = br.readLine();
+
+            if (s.equals("0")) break;
+
+            int len = s.length();
+
+            boolean isEqual = true;
+
+            for (int i = 0; i < len / 2; i++) {
+                if (s.charAt(i) != s.charAt(len - 1 - i)) {
+                    isEqual = false;
+                    break;
+                }
+            }
+
+            if (isEqual) {
+                res.append("yes\n");
+            } else {
+                res.append("no\n");
+            }
+        }
+
+        System.out.print(res);
+
     }
 }
