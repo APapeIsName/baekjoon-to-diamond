2c7a196896eea822d57dadb5c46bd52e30222191
APapeIsName <gms72901217@gmail.com>
2025-12-14 00:16:36 +0900

    문제 풀이 완료. 최적화 필요

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,31 @@
 import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    // 104ms, 수학적인 공식에 대해 더 생각해볼 필요가 있음.
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+
+    }
+
+    // 420ms
+    // 평균 280ms, 최고점 204ms 에 비해 터무니없이 느리므로 최적화 필요
+    private static void first() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int count = Integer.parseInt(br.readLine());
-        int[] numbers = new int[count];
-        int max = 0;
+        List<String> stringList = new ArrayList<>();
 
-        String[] numberSplit = br.readLine().split(" ");
         for (int i = 0; i < count; i++) {
-            numbers[i] = Integer.parseInt(numberSplit[i]);
-            if (numbers[i] > max) max = numbers[i];
+            stringList.add(br.readLine());
         }
 
-        double total = 0.0;
-        for (int i = 0; i < count; i++) {
-            total += ((double) numbers[i] / max);
-        }
+        String res = new HashSet<>(stringList)
+                .stream()
+                .sorted(Comparator.comparing(String::length)
+                        .thenComparing(String::stripTrailing))
+                .collect(Collectors.joining("\n"));
 
-        System.out.print((total / count) * 100);
+        System.out.print(res);
     }
 }
