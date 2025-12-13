572eac09f396b722d12e4fc723ae25f13731c894
APapeIsName <gms72901217@gmail.com>
2025-12-14 00:36:30 +0900

    문제 풀이 최적화, 고칠 점 작성

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,36 @@ import java.util.stream.Collectors;
 
 public class Main {
 
+    // 284~320ms
+    // 고칠 점 1. HashSet 은 많은 시간 복잡도를 잡아먹으니 사용하지 않기, 중복 제거는 마지막 반복문으로 2. sort, String.compareTo(String) 으로 비교해서 문자열 정렬시키기
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int count = Integer.parseInt(br.readLine());
+        String[] strArr = new String[count];
+
+        for (int i = 0; i < count; i++) {
+            strArr[i] = br.readLine();
+        }
+
+        Arrays.sort(strArr, (s1, s2) -> {
+            if (s1.length() == s2.length()) {
+                return s1.compareTo(s2);
+            }
+            return s1.length() - s2.length();
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(strArr[0]).append('\n');
+
+        for (int i = 1; i < count; i++) {
+            if (!strArr[i].equals(strArr[i-1])) {
+                sb.append(strArr[i]).append('\n');
+            }
+        }
+
+        System.out.print(sb);
     }
 
     // 420ms
