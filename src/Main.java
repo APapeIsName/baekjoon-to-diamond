450821b8a74679d6907d02c0fff712bdee2b035c
APapeIsName <gms72901217@gmail.com>
2025-12-13 23:23:12 +0900

    문제 풀이 완료. 보완할 점 크게 없음

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,25 @@
 import java.io.*;
 
 public class Main {
-    // 104ms
-    // 고쳐야 할 점 : 1.StringTokenizer 를 사용해보기
-    // 2.만약 범위가 늘어나면 안 좋은 코드가 될 수 있음. 이럴 땐 메모리를 써서 시간 복잡도를 해결해보기. -> 지금 거는 2번 for문을 돌게 됨 -> 범위가 늘어나면 안 좋아짐 -> 미리 마지막까지 돈 다음 거기서 배열 자르기
+
+    // 108ms, 약수의 공식을 이용한 풀이 방법.
+    // 참고할 점 : StringTokenizer 를 쓸 땐 데이터가 많을 때
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
 
-        String[] arr = br.readLine().split(" ");
-        int first = Integer.parseInt(arr[0]);
-        int last = Integer.parseInt(arr[1]);
+        int nCount = Integer.parseInt(br.readLine());
 
-        int count = 0;
-        int num = 1;
-        int firstSum = 0;
+        String[] numbersArr = br.readLine().split(" ");
 
-        for (int i = 1; i < first; i++) {
-            if (count == num) {
-                count = 0;
-                num++;
-            }
-            firstSum += num;
-            count++;
+        int min = 9999999;
+        int max = -1;
+
+        for (int i = 0; i < nCount; i++) {
+            int now = Integer.parseInt(numbersArr[i]);
+            if (now > max) max = now;
+            if (now < min) min = now;
         }
 
-        count = 0;
-        num = 1;
-        int lastSum = 0;
-
-        for (int i = 1; i <= last; i++) {
-            if (count == num) {
-                count = 0;
-                num++;
-            }
-            lastSum += num;
-            count++;
-        }
-
-        System.out.print(lastSum - firstSum);
+        System.out.print(max * min);
     }
 }
