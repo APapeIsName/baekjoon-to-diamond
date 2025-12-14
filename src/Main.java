b52e7c3b7104ecccfd43efa503a23e60bf8c36be
APapeIsName <gms72901217@gmail.com>
2025-12-15 00:07:39 +0900

    문제 풀이 완료.

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,31 @@
 import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    // 284~320ms
-    // 고칠 점 1. HashSet 은 많은 시간 복잡도를 잡아먹으니 사용하지 않기, 중복 제거는 마지막 반복문으로 2. sort, String.compareTo(String) 으로 비교해서 문자열 정렬시키기
+    // 108ms. 크기가 클 경우엔 배열로 바꿔야 한다는 생각을 갖고 있어야 함
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int count = Integer.parseInt(br.readLine());
-        String[] strArr = new String[count];
+
+        int res = 0;
 
         for (int i = 0; i < count; i++) {
-            strArr[i] = br.readLine();
-        }
-
-        Arrays.sort(strArr, (s1, s2) -> {
-            if (s1.length() == s2.length()) {
-                return s1.compareTo(s2);
+            List<Character> chList = new ArrayList<>();
+            String word = br.readLine();
+            boolean isExists = false;
+            chList.add(word.charAt(0));
+            for (int j = 1; j < word.length(); j++) {
+                if (word.charAt(j) != word.charAt(j-1)) {
+                    isExists = chList.contains(word.charAt(j));
+                    if (isExists) break;
+                    chList.add(word.charAt(j));
+                }
             }
-            return s1.length() - s2.length();
-        });
-
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(strArr[0]).append('\n');
-
-        for (int i = 1; i < count; i++) {
-            if (!strArr[i].equals(strArr[i-1])) {
-                sb.append(strArr[i]).append('\n');
-            }
+            if (!isExists) res++;
         }
-
-        System.out.print(sb);
-    }
-
-    // 420ms
-    // 평균 280ms, 최고점 204ms 에 비해 터무니없이 느리므로 최적화 필요
-    private static void first() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int count = Integer.parseInt(br.readLine());
-        List<String> stringList = new ArrayList<>();
-
-        for (int i = 0; i < count; i++) {
-            stringList.add(br.readLine());
-        }
-
-        String res = new HashSet<>(stringList)
-                .stream()
-                .sorted(Comparator.comparing(String::length)
-                        .thenComparing(String::stripTrailing))
-                .collect(Collectors.joining("\n"));
 
         System.out.print(res);
     }
